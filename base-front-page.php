0082500f44245a7fbab9611d32bc25197ce18fec
Mason L <mason.lawlor@me.com>
2014-11-12 18:54:54 -0800

    copy changes, email optin list change

--- base-front-page.php
+++ base-front-page.php
@@ -30,8 +30,8 @@
   		<div class="col-xs-12 text-center">
 	      <a href="//fast.wistia.net/embed/iframe/va8o82dilx?popover=true" class="wistia-popover[height=360,playerColor=ffffff,width=640]"><img src="https://embed-ssl.wistia.com/deliveries/b1d475a810d5c1b174c980e1e3ee033985c16c7a.jpg?image_play_button=true&image_play_button_color=ffffffe0&image_crop_resized=150x84" alt="" /></a>
 	      <script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
-	  	    <h1>We build sexy Wordpress sites that are <span class="highlight">damn fast</span>.</h1>
-	  	    <h3>Either we'll do it for you, or we'll teach you how.</h2>
+	  	    <h1>Learn how to build Wordpress sites that are <span class="highlight">damn fast</span>.</h1>
+	  	    <h3>Or you can skip the work and just buy one.</h2>
 	  	  <a href="#products" rel='m_PageScroll2id' type="button" type="button" class="btn btn-lg btn-primary" data-toggle="modal" data-target="#myModal">Custom themes<i class="glyphicon glyphicon-chevron-down"></i></a>
   		</div>
   	</div> <!-- /Video -->
@@ -44,23 +44,23 @@
 	  <div class="row">
 		  <div class="col-sm-4 col-xs-12 text-center">
 			  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/G-Strap.png" class="img-responsive" alt="G-Strap" width="313" height="178" />
-			  <h3>G-String</h3>
-			  <p>G-String is our bad bitch. Beginners will love all of her widgets because they make her so easy to manipulate. You won't need to use much PHP, and most of the customization will take place in her visual editor. Her only downfall is that she's <a href="http://jackalope.io/g-string/" alt="g-string theme">harder to backup.</a></p>
-			  <a href="<?php echo esc_url(home_url('/')); ?>g-string/" class="btn btn-lg btn-primary">Demo<i class="glyphicon glyphicon-chevron-right"></i></a>
+			  <h3>G-Strap</h3>
+			  <p>iTunes Apple Store Apple Pay ever better HDR camera. Camera performance Mavericks best iBooks. Upshot personality Spotlight $799 design did LTE. Selfie touchscreen 3X faster $599 design iPhone teraflops. Screen touchscreen HealthKit $249 hi-def. iCloud Apple Store teraflops Jony. Upshot thunderbolt dark mode.</p>
+			  <a href="<?php echo esc_url(home_url('/')); ?>g-strap/" class="btn btn-lg btn-primary">Live demo<i class="glyphicon glyphicon-chevron-right"></i></a>
 			  <p class="post-btn">Starting @ $199</p>
 		  </div>
 		  <div class="col-sm-4 col-xs-12 text-center">
 			  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Nudist.png" class="img-responsive" alt="Nudist" width="313" height="178" />
 			  <h3>Nudist</h3>
-			  <p>Nudist doesn't fuck around. When he's first installed, he looks completely naked. But don't underestimate him. Once he's all greased up he transforms into the most powerful Wordpress theme on earth. Nudist is beginner friendly, but not for the faint of heart.</p>
-			  <a href="<?php echo esc_url(home_url('/')); ?>nudist/" class="btn btn-lg btn-primary">Demo<i class="glyphicon glyphicon-chevron-right"></i></a>
+			  <p>Slo-mo translucent Downloads Jony design 1080p. Hi-def application until now $199 pixels upshot. Finder performance one touch love design LTE camera A8X. Slo-mo power users Apple Pay iCal better upshot Mavericks. Selfie translucent brilliant Siri upshot. Better Apple Store one touch iMac pixels 5mm change HDR.</p>
+			  <a href="<?php echo esc_url(home_url('/')); ?>nudist/" class="btn btn-lg btn-primary">Live demo<i class="glyphicon glyphicon-chevron-right"></i></a>
 			  <p class="post-btn">Starting @ $249</p>
 		  </div>
 		  <div class="col-sm-4 col-xs-12 text-center">
 			  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Custom.png" class="img-responsive" alt="Custom" width="313" height="178" />
 			  <h3>Custom</h3>
-			  <p>Don't have the time to learn Wordpress development and web design? Our agency will take care of you over at <a href="http://jackalopeMedia.com/" alt="Jackalope Media">JackalopeMedia.com</a>. We build long term relationships with badass business people so we can align our expectations and thrive.</p>
-			  <button type="submit" class="btn btn-lg btn-primary">Demo<i class="glyphicon glyphicon-chevron-right"></i></button>
+			  <p>Change fingerprint 3X faster $699 iBooks. Finder remote lock Mavericks thin retina Air. Slo-mo thunderbolt iPhone 6+ thin Finder upshot until now. Upshot Apple Store Spotlight $499 selfie AMD hi-def 240 fps. iTunes power users. Upshot Smart Cover App Store love Finder HDR LTE. Unlock thunderbolt iPhone Pages 3X.</p>
+			  <button type="submit" class="btn btn-lg btn-primary">Services<i class="glyphicon glyphicon-chevron-right"></i></button>
 			  <br><a class="view-examples" href="#">View examples</a>
 		  </div>
 	  </div>
